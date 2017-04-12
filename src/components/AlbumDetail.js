9e89c1ac36bfe7c73fffe021dd800f85c6961317
kimwah <kimwahwong@gmail.com>
2017-04-12 19:50:42 +0800

    Using React Linking component to fire Browser

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 import Button from './Button';
@@ -25,7 +25,9 @@ const AlbumDetail = (props) => {
       </CardSection>   
 
       <CardSection>
-         <Button onPress={() => console.log(props.album.title)} />
+         <Button onPress={() => Linking.openURL(props.album.url)}>
+             Buy Now
+         </Button>
       </CardSection>       
     </Card>
   );
